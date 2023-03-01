4fcfc821bfe96e520ec66c70d4ed33b1b752fc45
mayudong200333 <mayudong200333@gmail.com>
2023-03-01 05:11:09 -0800

    deleted some files

--- build/examples_rclpy_minimal_client/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/mayudong/ros2_ws/install/examples_rclpy_minimal_client'
--- build/examples_rclpy_minimal_client/setup.py
+++
@@ -1,1 +0,0 @@
-/home/mayudong/ros2_ws/src/examples/rclpy/services/minimal_client/setup.py
